3c337f2077d55e2760092a50d0c5de3fc4bcc76b
Neil Javiya <41284894+neiljaviya@users.noreply.github.com>
2021-03-10 12:08:24 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import 'firebase/firestore'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyDo1MJURnDA4bAwuOO5gmGX5iSNwt4hBng",
-    authDomain: "vue-calendar-d6a05.firebaseapp.com",
-    projectId: "vue-calendar-d6a05",
-    storageBucket: "vue-calendar-d6a05.appspot.com",
-    messagingSenderId: "193268396518",
-    appId: "1:193268396518:web:4e55f6c87c140f82962627"
+    //firebase config here
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
